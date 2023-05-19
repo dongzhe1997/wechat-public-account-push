898f072168708d5975f1b262c55f04218bdcfa5b
dongzhe1997 <39683718+dongzhe1997@users.noreply.github.com>
2023-05-19 15:33:21 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o5D7o5hP3fRtZJJxqvbBABsKsU1w',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '2BK1Qhk8lqQsA7WBgLSYTuPOS17rxFGMiEWLj4sv-AI',
+      useTemplateId: 'yVcoAHUXg_U0W10dLpEFWLGXZ213vuFUv96TYFeXC9w',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '2BK1Qhk8lqQsA7WBgLSYTuPOS17rxFGMiEWLj4sv-AI',
+  CALLBACK_TEMPLATE_ID: 'yVcoAHUXg_U0W10dLpEFWLGXZ213vuFUv96TYFeXC9w',
 
   CALLBACK_USERS: [
     {
