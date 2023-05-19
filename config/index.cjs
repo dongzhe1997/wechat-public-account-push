c0ef537d086fb8001b4525f999f6b6bba87bfa9e
dongzhe1997 <39683718+dongzhe1997@users.noreply.github.com>
2023-05-19 16:21:28 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '臭宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o5D7o5tIPYriYJQLK3H8lk6GH-q4',
+      id: 'o5D7o5hP3fRtZJJxqvbBABsKsU1w',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'qVrYz09BWhT76QZ6QGxkaojyNKEvByUWvJPi4FE1ekc',
+      useTemplateId: 'HfEGKM5orEhL2-TXZdZ3kkjb2-hFF8IsfLZl2YfEHuE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
@@ -42,7 +42,7 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2020-07-04' },
         // 结婚纪念日
         { keyword: 'marry_day', date: '2022-09-09' },
       ],
