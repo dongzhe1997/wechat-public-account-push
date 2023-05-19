fc510efaaba03928ec400048fef5ab9dbc5ac28c
dongzhe1997 <39683718+dongzhe1997@users.noreply.github.com>
2023-05-19 15:06:28 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -55,9 +55,9 @@ const USER_CONFIG = {
 
   CALLBACK_USERS: [
     {
-      name: 'o5D7o5tIPYriYJQLK3H8lk6GH-q4',
+      name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'o5D7o5tIPYriYJQLK3H8lk6GH-q4',
     }
   ],
 
